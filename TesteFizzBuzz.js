c22191b911e148adc295474617c0307449c439b5
vbarbos <vbarbos@users.noreply.github.com>
2017-04-20 20:59:00 -0300

    Subida para deixar o código em um arquivo só

--- TesteFizzBuzz.js
+++ TesteFizzBuzz.js
@@ -25,3 +25,23 @@ function fizzBuzz() {
 
 var results = fizzBuzz();
 console.log(lista);
+
+test("Retorna o total de itens", function() {
+    equal(results.length, 100);
+});
+
+test("verificação do primeiro resultado igual a 1", function() {
+    equal(results[0], 1);
+});
+
+test("print do fizz no número 3", function() {
+    equal(results[2], "Fizz");
+});
+
+test("print do buzz no número 5", function() {
+    equal(results[4], "Buzz");
+});
+
+test("print do fizzbuzz no número 15", function() {
+    equal(results[14], "FizzBuzz");
+});
