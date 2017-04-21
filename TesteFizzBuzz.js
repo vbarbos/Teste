865791634c9c1f5f68216d26b377f314b5d885d4
vbarbos <vbarbos@users.noreply.github.com>
2017-04-20 21:01:02 -0300

    Update TesteFizzBuzz.js

--- TesteFizzBuzz.js
+++ TesteFizzBuzz.js
@@ -27,21 +27,21 @@ var results = fizzBuzz();
 console.log(lista);
 
 test("Retorna o total de itens", function() {
-    equal(results.length, 100);
+    equal(lista.length, 100);
 });
 
 test("verificação do primeiro resultado igual a 1", function() {
-    equal(results[0], 1);
+    equal(lista[0], 1);
 });
 
 test("print do fizz no número 3", function() {
-    equal(results[2], "Fizz");
+    equal(lista[2], "Fizz");
 });
 
 test("print do buzz no número 5", function() {
-    equal(results[4], "Buzz");
+    equal(lista[4], "Buzz");
 });
 
 test("print do fizzbuzz no número 15", function() {
-    equal(results[14], "FizzBuzz");
+    equal(lista[14], "FizzBuzz");
 });
