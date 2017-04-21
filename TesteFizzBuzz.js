fd5d1a3d24d3486291c65230fa99ad0d1bad7698
vbarbos <vbarbos@users.noreply.github.com>
2017-04-20 22:12:32 -0300

    Update TesteFizzBuzz.js
    ajuste para a visualizaçào do teste em browser

--- TesteFizzBuzz.js
+++ TesteFizzBuzz.js
@@ -1,47 +1,47 @@
 function fizzBuzz() {
-    var lista = [];
+    var resultado = [];
     for (i = 0; i < 100; i++) {
         var value = i + 1;
-        
+
         if (value % 3 == 0) {
-            lista[i] = "Fizz";
+            resultado[i] = "Fizz";
             continue;
         }
         if (value % 5 == 0) {
-            lista[i] = "Buzz";
-            continue;
-        }
-        
-        if (value % 3 == 0 && value % 5 == 0) {
-            lista[i] = "FizzBuzz";
+            resultado[i] = "Buzz";
             continue;
         }
 
-        if (lista[i] == undefined) lista[i] = value;
+        if (value % 3 == 0 && value % 5 == 0) {
+            resultado[i] = "FizzBuzz";
+            continue;
+        }
+
+        if (resultado[i] == undefined) resultado[i] = value;
     }
-    return lista;
+    return resultado;
 };
 
 
 var resultado = fizzBuzz();
-console.log(lista);
+console.log(resultado);
 
-test("Retorna o total de itens", function() {
-    equal(resultado.length, 100);
+QUnit.test("Retorna o total de itens", function(assert) {
+    assert.ok(resultado.length, 100);
 });
 
-test("verificação do primeiro resultado igual a 1", function() {
-    equal(resultado[0], 1);
+QUnit.test("verificação do primeiro resultado igual a 1", function(assert) {
+    assert.ok(resultado[0], 1);
 });
 
-test("print do fizz no número 3", function() {
-    equal(resultado[2], "Fizz");
+QUnit.test("print do fizz no número 3", function(assert) {
+    assert.ok(resultado[2], "Fizz");
 });
 
-test("print do buzz no número 5", function() {
-    equal(resultado[4], "Buzz");
+QUnit.test("print do buzz no número 5", function(assert) {
+    assert.ok(resultado[4], "Buzz");
 });
 
-test("print do fizzbuzz no número 15", function() {
-    equal(resultado[14], "FizzBuzz");
+QUnit.test("print do fizzbuzz no número 15", function(assert) {
+    assert.ok(resultado[14], "FizzBuzz");
 });
